32a61035c60662c1425797747ad5fa66176f82b8
Alvaro Lima <alvaromlima@gmail.com>
2022-07-21 16:29:44 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,7 +5,6 @@ using xadrez;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            /*
             try {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
@@ -13,19 +12,23 @@ namespace xadrez_console {
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 7));
+
                 Tela.imprimirTabuleiro(tab);
 
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
             }
-            */
 
+            /*
             PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
             Console.WriteLine(pos);
 
             Console.WriteLine(pos.toPosicao());
+            */
 
             Console.ReadLine();
 
